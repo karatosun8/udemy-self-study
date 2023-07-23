5ba648d3ca003f43f3b7b04318ea0676fdb96cab
karatosun8 <sefa_karatosun@hotmail.com>
2023-07-23 14:10:19 +0300

    ...

---
+++ study-1/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  const name ="sefa"
+  const students = 12000
+  const dogruMu =true
+  return (
+    // <div className="App">
+    //   <h1>Adı = {name} </h1>
+    //   <input type="text"/>
+    //   <img src="https://picsum.photos/250/155"/>
+
+
+    //   {dogruMu ? <p>{students}</p> : <p>yanlış</p>}
+    //   <input type="date"/>
+
+    // </div>
+    <div className="box">
+
+    </div>
+  );
+}
+
+export default App;
