cf806ed1563fbb379a553ac9f269d749ce2ba80f
karatosun8 <sefa_karatosun@hotmail.com>
2023-07-26 21:00:43 +0300

    themeContext

--- library/src/App.js
+++ library/src/App.js
@@ -1,10 +1,19 @@
-import AppRouter from './router/AppRouter';
+import AppRouter from "./router/AppRouter";
+import { ThemeProvider } from "styled-components";
+import { lightTheme, darktheme } from "./styles/theme";
+import { GlobalStyles } from "./styles/Global.styles";
+import ThemeContextProvider, { useThemeContext } from "./context/ThemeContext";
 
 function App() {
+  const{myTheme}= useThemeContext()
   return (
-    <div className="App">
-      <AppRouter/>
-    </div>
+    <>
+      <ThemeProvider theme={lightTheme}>
+        <GlobalStyles />
+        <AppRouter />
+      </ThemeProvider>
+      </>
+    
   );
 }
 
