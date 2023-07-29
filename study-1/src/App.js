5c0165a4819492175cfd663a535a9303534376a6
karatosun8 <sefa_karatosun@hotmail.com>
2023-07-29 22:51:26 +0300

    css

--- study-1/src/App.js
+++ study-1/src/App.js
@@ -1,30 +1,17 @@
 
 import './App.css';
+import TaskCreate from './components/TaskCreate';
 
 function App() {
 
-  const name ="sefa"
-  const students = 12000
-  const dogruMu =true
+  
   return (
-    // <div className="App">
-    //   <h1>Adı = {name} </h1>
-    //   <input type="text"/>
-    //   <img src="https://picsum.photos/250/155"/>
+    <div className='App'>
 
-
-    //   {dogruMu ? <p>{students}</p> : <p>yanlış</p>}
-    //   <input type="date"/>
-
-    // </div>
-    <div style= {{
-      backgroundColor:"yellowgreen",
-      width:"300px",
-      height:"300px"
-
-    }}>
-
+      <TaskCreate/>
     </div>
+
+    
   );
 }
 
