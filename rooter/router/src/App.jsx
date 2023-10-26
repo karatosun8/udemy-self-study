ef501cbd6511ef6e0ef390418e4a71b6e3cedd5a
karatosun8 <sefa_karatosun@hotmail.com>
2023-10-26 12:09:38 +0300

    Navlink

--- rooter/router/src/App.jsx
+++ rooter/router/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import {Routes , Route} from "react-router-dom"
 import Home from './components/Home'
 import AboutUs from './components/AboutUs'
+import Navbar from './components/Navbar'
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
 
   return (
     <>
+    <Navbar/>
     <Routes>
       <Route path='/' element={<Home/>} />
       <Route path='/aboutUs' element={<AboutUs/>} />
